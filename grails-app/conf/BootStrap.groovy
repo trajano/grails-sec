9912a106b327673299977c13a2163f95b158a58a
Archie <arch@trajano.net>
2014-08-08 12:54:42 -0400

    Added roles and allowed access to admin user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,9 +1,13 @@
+import com.yourapp.Role
 import com.yourapp.User
+import com.yourapp.UserRole
 class BootStrap {
 
     def init = { servletContext ->
 	if (User.count() == 0) {
-            new User(username: "trajano", password: "trajano").save(failOnError: true)
+            def adminUser = new User(username: "trajano", password: "trajano").save(failOnError: true)
+            UserRole.create adminUser, new Role(authority: "ROLE_ADMIN").save(failOnError: true)
+            UserRole.create adminUser, new Role(authority: "ROLE_USER").save(failOnError: true)
         }
     }
     def destroy = {
