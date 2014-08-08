96427bf67a581932bd90dced41386c6105284223
Archie <arch@trajano.net>
2014-08-08 12:13:58 -0400

    Created initial user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,10 @@
+import com.yourapp.User
 class BootStrap {
 
     def init = { servletContext ->
+	if (User.count() == 0) {
+            new User(username: "trajano", password: "trajano").save(failOnError: true)
+        }
     }
     def destroy = {
     }
